1e9068d1a028b2627c49dbe83fb6cf527d4ba832
thomasyp <yangpu@sinap.ac.cn>
2016-03-25 16:30:47 +0800

    euler method

--- test.py
+++ test.py
@@ -1,25 +1,19 @@
-# -*- coding: utf-8 -*-
-from scipy.integrate import odeint 
-import numpy as np 
+#Euler method
+import numpy as np
 
-def lorenz(w, t, p, r, b): 
-    # 给出位置矢量w，和三个参数p, r, b计算出
-    # dx/dt, dy/dt, dz/dt的值
-    x, y, z = w
-    # 直接与lorenz的计算公式对应 
-    return np.array([p*(y-x), x*(r-z)-y, x*y-b*z]) 
+def Eulermethod(fuc,h,l,x_0,y_0):
+	n = l/h
+	x_p = x_0
+	y_p = y_0
+	for i in range(int(n)):
+		y_n = y_p + h * fuc(x_p,y_p)
+		x_p = x_p + h 
+		y_p = y_n
+		print "x_p:",x_p,"y_p:",y_p
+		print y_n
+	
 
-t = np.arange(0, 30, 0.01) # 创建时间点 
-# 调用ode对lorenz进行求解, 用两个不同的初始值 
-track1 = odeint(lorenz, (0.0, 1.00, 0.0), t, args=(10.0, 28.0, 3.0)) 
-track2 = odeint(lorenz, (0.0, 1.01, 0.0), t, args=(10.0, 28.0, 3.0)) 
-
-# 绘图
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt 
-
-fig = plt.figure()
-ax = Axes3D(fig)
-ax.plot(track1[:,0], track1[:,1], track1[:,2])
-ax.plot(track2[:,0], track2[:,1], track2[:,2])
-plt.show()
+def testfuc(x,y):
+	return x*x*y
+	
+Eulermethod(testfuc,0.1,0.5,0,1)
